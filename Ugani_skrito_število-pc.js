e73821b040a6eb52c167bb78ebddb18668eb483b
Jernej Domonkoš <domonkos.jernej@gmail.com>
2015-12-01 07:36:00 +0100

    dodano Ugani_skrito_število-pc

---
+++ Ugani_skrito_število-pc.js
@@ -0,0 +1,27 @@
+var x = 1; // najnižja začetna omejitev števila
+var y = 100; // najvišja začetna omejitev
+var hidden = prompt("Vnesi skrito stevilo (od 1 do 100): " );  
+var result = [];
+var number = Math.floor(Math.random() * (y-x+1)+ x); // funkcija generiranja naključnega števila z uporabo omejitev (x,y)
+
+	for (var i =1; i<=100; i++)
+	{   
+		if (hidden != number)
+		{	
+        if (hidden > number)
+            {
+              x=number;
+              number = Math.floor((Math.random() * y) + x);
+            }
+    	if (hidden < number)
+            {
+              y=number;
+              number = Math.floor((Math.random() * y) + x);
+            }
+       
+            result.push(number); 
+		}
+	}
+							
+        console.log('Ugibanja računalnika: ' + result);
+        console.log('Sktiro stevilo je bilo: ' + hidden);
